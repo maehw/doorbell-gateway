fbc9039fcf8d04bab5465346862bfa8b84b05c3c
maehw <matthiaz.weber@gmx.de>
2020-03-30 21:27:23 +0200

    Fixed comment in config.h

--- config.h
+++ config.h
@@ -8,9 +8,7 @@
 #define ANALOG_INPUT_PIN    A0
 
 
-/* Sensor loop delay to busy wait and currently get a sample interval of approx. 2.5 ms,
- * i.e. a sampling frequency of 400 Hz.
- */
+/* Sensor loop delay to busy wait and currently get a sample interval of approx. 3 ms, i.e. a sampling frequency of 333 Hz. */
 #define SENSOR_LOOP_DELAY       (3u)
 
 #define SENSOR_OFFSET_VOLTAGE   (3835.0f)
