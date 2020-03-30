56e49f22e6f14b2fa2e2e189cd5348e4c671ace0
maehw <matthiaz.weber@gmx.de>
2020-03-30 21:24:42 +0200

    Working demo on NodeMCU 1.0 ESP-12E

---
+++ config.h
@@ -0,0 +1,20 @@
+#define WIFI_SSID           "MyWifiSsid"
+#define WIFI_PASSWORD       "MyWifiPassword"
+#define MQTT_SERVER         "MyLocalIp"
+#define MQTT_PORT           1883
+#define MQTT_TOPIC          "Test"
+#define MQTT_MSG_CONNECT    "Connected"
+#define MQTT_MSG_DOORBELL   "Ding-dong"
+#define ANALOG_INPUT_PIN    A0
+
+
+/* Sensor loop delay to busy wait and currently get a sample interval of approx. 2.5 ms,
+ * i.e. a sampling frequency of 400 Hz.
+ */
+#define SENSOR_LOOP_DELAY       (3u)
+
+#define SENSOR_OFFSET_VOLTAGE   (3835.0f)
+
+#define SENSOR_THRESHOLD        (40.0f)
+
+#define SENSOR_CNT_LIMIT        (20)
--- doorbell-gateway.ino
+++ doorbell-gateway.ino
@@ -1,102 +1,87 @@
-// Test setup:
-//   Arduino Nano pin asssignment:
-//     Analog input: A0
-//     LED: 13 (on board LED)
-
-#define LED_PIN               (13)
-
-/* Mapping of the analog input pin, define alias here */
-#define ANALOG_INPUT_PIN      (A0)
-
-/* Mapping of the digital output pin for real-time supervision, define alias here */
-#define RT_SUPERVISION_PIN    (A3)
-
-/* Sensor loop delay to busy wait and currently get a sample interval of approx. 2.5 ms,
- * i.e. a sampling frequency of 400 Hz.
+/*
+ * MQTT based doorbell gateway  
+ * 
+ * It connects to an MQTT server in the local network.
+ * It tries to detect the sound of the doorbell and then publishes 
+ * a message (e.g. "ding-dong") to an arbitrary topic (e.g. "Test").
+ * It currently also sends a message on (re-)connect.
+ * 
+ * This has beentested on a NodeMCU 1.0 ESP12-E.
  */
-#define SENSOR_LOOP_DELAY     (2u)
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these configuration values suitable for your network.
+#include "config.h"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void setup_wifi()
+{
+    delay(10);
+
+    // We start by connecting to a WiFi network
+    Serial.println();
+    Serial.print("Connecting to WiFi with SSID: \"");
+    Serial.print(WIFI_SSID);
+    Serial.println("\"");
+  
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+  
+    Serial.println("");
+    Serial.println("WiFi connected.");
+
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconnect()
+{
+    // Loop until we're reconnected
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+
+        // Attempt to connect
+        if (client.connect("RmClientWithoutSpe", "", ""))
+        {
+            Serial.println("connected");
+
+            // Once connected, publish an announcement...
+            client.publish(MQTT_TOPIC, MQTT_MSG_CONNECT);
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" trying again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
 
 void setup()
 {
+    pinMode(BUILTIN_LED, OUTPUT); // initialize the BUILTIN_LED pin as an output
     Serial.begin(230400); // start serial for debug output
+    setup_wifi();
 
-#ifdef RT_SUPERVISION_PIN
-    pinMode(RT_SUPERVISION_PIN, OUTPUT);
-#endif
+    client.setServer(MQTT_SERVER, MQTT_PORT);
 
-    pinMode(LED_PIN, OUTPUT);
-}
-
-void loop()
-{
-    static int nConversionValue = 0;
-    static int nCnt = 0;
-    static int nCntDown = 0;
-    static int nOutput = 0;
-    static float fVoltage = 0.0f;
-    static float fSignal = 0.0f;
-    //const float fOffset = 2710.0f; // threshold for connected logic analyzer
-    const float fOffset = 2755.0f;
-    const float fThreshold = 40.0f;
-
-#ifdef RT_SUPERVISION_PIN
-    digitalWrite(RT_SUPERVISION_PIN, HIGH);
-#endif
-
-    nConversionValue = analogRead(ANALOG_INPUT_PIN);
-    fVoltage = countsToMillivolts(nConversionValue);
-    fSignal = fVoltage - fOffset;
-
-//    Serial.print("Conversion value: ");
-//    Serial.println(nConversionValue);
-
-    if( abs(fSignal) > fThreshold )
+    if (!client.connected())
     {
-        nCnt++;
+        reconnect();
     }
-    else
-    {
-        nCnt--;
-        if( nCnt < 0 )
-        {
-            nCnt = 0;
-        }
-    }
-
-    if( nCnt > 20 )
-    {
-        // switch on
-        nOutput = 100; // arbitrary value for plot scaling
-        nCntDown = 400;
-        digitalWrite(LED_PIN, HIGH);
-    }
-
-    if( nCntDown > 0 )
-    {
-        nCntDown--;
-        if( nCntDown == 0 )
-        {
-            // switch off
-            nOutput = 0;
-            digitalWrite(LED_PIN, LOW);
-        }
-    }
-
-    //Serial.print("Sig: ");
-    //Serial.print( fSignal );
-    Serial.print(", |Sig|: ");
-    Serial.print( abs(fSignal) );
-    Serial.print(", Count: ");
-    Serial.print( nCnt );
-    Serial.print(", Out: ");
-    Serial.println( nOutput );
-
-#ifdef RT_SUPERVISION_PIN
-    digitalWrite(RT_SUPERVISION_PIN, LOW);
-#endif
-
-    /* Wait until next cyclic measurement is made */
-    delay(SENSOR_LOOP_DELAY);
 }
 
 float countsToMillivolts(int nCounts)
@@ -106,3 +91,93 @@ float countsToMillivolts(int nCounts)
     // This yields a resolution between readings of: 5 volts / 1024 units or approx. 4.9 mV per LSB
     return nCounts * 4.882813f;
 }
+
+bool detectDoorbell(float fVoltage)
+{
+    const float fOffset = SENSOR_OFFSET_VOLTAGE;
+    const float fThreshold = SENSOR_THRESHOLD;
+    static float fSignal;
+    static int nCnt = 0;
+    static int nCntDown = 0;
+    static bool bDetected = false;
+    static bool bDetectedOld = false;
+
+    fSignal = fVoltage - fOffset;
+
+    if( abs(fSignal) > fThreshold )
+    {
+        nCnt++; // increase likeliness of occurence
+    }
+    else
+    {
+        nCnt--; // decrease likeliness of occurence, but never fall below zero
+        if( nCnt < 0 )
+        {
+            nCnt = 0;
+        }
+    }
+
+    if( nCnt > SENSOR_CNT_LIMIT )
+    {
+        // detected
+        bDetected = true;
+
+        // keep output true for specific time
+        nCntDown = 400;
+    }
+
+    if( nCntDown > 0 )
+    {
+        nCntDown--;
+        if( nCntDown == 0 )
+        {
+            // no longer keep output true
+            bDetected = false;
+        }
+    }
+
+    Serial.print("|Sig|: ");
+    Serial.print( abs(fSignal) );
+    Serial.print(", Count: ");
+    Serial.print( nCnt );
+
+    return bDetected;
+}
+
+void loop()
+{
+    static int nConversionValue = 0;
+    static bool bDetected = false;
+    static bool bDetectedOld = false;
+    
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+
+    nConversionValue = analogRead(ANALOG_INPUT_PIN);
+
+    bDetected = detectDoorbell( countsToMillivolts(nConversionValue) );
+
+    Serial.print(", Out: ");
+    Serial.println( bDetected ? 300 : 0 );
+
+    /* detect rising edge */
+    if( (bDetectedOld != bDetected) && bDetected )
+    {
+        if( client.publish(MQTT_TOPIC, MQTT_MSG_DOORBELL) )
+        {
+            Serial.println("Published via MQTT.");
+        }
+        else
+        {
+            Serial.println("Unable to publish via MQTT.");
+        }
+    }
+    bDetectedOld = bDetected;
+
+    delay(SENSOR_LOOP_DELAY);
+}
+
+
